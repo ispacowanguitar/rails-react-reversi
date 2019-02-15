36e04e75329809f0e7e5a05ac34d4be44f8046f1
Alex Ispa-Cowan <ispacowanguitar@gmail.com>
2019-02-15 14:59:11 -0800

    Move javascript tests to spec directory and get rid of test console errors.

--- spec/javascript/__tests__/Game-test.js
+++ spec/javascript/__tests__/Game-test.js
@@ -2,11 +2,12 @@ import React from "react";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import { shallow } from "enzyme";
-import Game from "../Game";
+import Game from "reversiApp/components/Game";
 
 configure({ adapter: new Adapter() });
 describe("<Game />", () => {
-  it("renders", () => {
+  it("Renders a ", () => {
     const wrapper = shallow(<Game />);
+    expect(1).toEqual(1);
   });
 });
