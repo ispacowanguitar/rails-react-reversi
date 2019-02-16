45ff0ed52e311582343dcff97f96b1c16c5d93a8
Alex Ispa-Cowan <ispacowanguitar@gmail.com>
2019-02-16 14:03:07 -0800

    Refactor to separate rows and columns

--- app/javascript/packs/reversiApp/components/Game.jsx
+++ app/javascript/packs/reversiApp/components/Game.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Square from "components/Square";
-
-const NUMBER_OF_SQUARES = 64;
-
+const NUMBER_OF_SQUARES = 8;
+const NUMBER_OF_ROWS = 8;
 class Game extends React.Component {
   render() {
     return (
       <>
-        {[...Array(NUMBER_OF_SQUARES)].map((x, index) => {
-          return <Square key={index} />;
+        {[...Array(NUMBER_OF_SQUARES)].map((x, row) => {
+          return [...Array(NUMBER_OF_ROWS)].map((x, column) => {
+            return <Square key={`${row}-${column}`} />;
+          });
         })}
       </>
     );
