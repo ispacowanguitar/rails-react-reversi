2fc67258e86ebf3ccefdf6602d82636d30f2cb69
Alex Ispa-Cowan <ispacowanguitar@gmail.com>
2019-02-16 10:15:04 -0800

    Jest setup to run on any file change and move enzyme setup to testSetup

--- spec/javascript/__tests__/Game-test.js
+++ spec/javascript/__tests__/Game-test.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
 import { shallow } from "enzyme";
 import Game from "components/Game";
 
-configure({ adapter: new Adapter() });
 describe("<Game />", () => {
   it("Renders a square component", () => {
     const wrapper = shallow(<Game />);
