0e480c74edff17f3c23b08b051969394ea38e317
Alex Ispa-Cowan <ispacowanguitar@gmail.com>
2019-02-16 14:55:54 -0800

    Finished board and found some pieces to use.

--- app/javascript/packs/reversiApp/components/Game.jsx
+++ app/javascript/packs/reversiApp/components/Game.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Square from "components/Square";
 const NUMBER_OF_SQUARES = 8;
 const NUMBER_OF_ROWS = 8;
+
 class Game extends React.Component {
   render() {
     return (
-      <div class="board">
+      <div className="board">
         {[...Array(NUMBER_OF_SQUARES)].map((x, row) => {
           return [...Array(NUMBER_OF_ROWS)].map((x, column) => {
             return <Square key={`${row}-${column}`} />;
