b4481267614814db47a0f5b4af8b61e197172991
Alex Ispa-Cowan <ispacowanguitar@gmail.com>
2019-02-16 13:35:07 -0800

    Render 64 styles squares

--- app/javascript/packs/reversiApp/components/Square.jsx
+++ app/javascript/packs/reversiApp/components/Square.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Square = props => {
-  return <div />;
+  return <div className="square" />;
 };
 
 export default Square;
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,7 +5,7 @@ module: {
       exclude: /node_modules/,
       loader: "babel-loader",
       options: {
-        presets: [["env", { modules: false }]]
+        presets: [["@babel/preset-env", { modules: false }]]
       }
     }
   ];
