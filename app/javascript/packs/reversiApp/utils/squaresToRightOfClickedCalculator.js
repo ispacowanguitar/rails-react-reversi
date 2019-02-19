43dbc9ab887b2201c7163837c4685aa42e86ea0a
Alex <Alex@Macbooks-MacBook-Pro-10.local>
2019-02-18 17:52:52 -0800

    Move squares above calculator to separate file

---
+++ app/javascript/packs/reversiApp/utils/squaresToRightOfClickedCalculator.js
@@ -0,0 +1,43 @@
+const INDEX_OF_FURTHEST_RIGHT_COLUMN = 7;
+
+export const flipSandwichedSquaresToRightOfClickedSquare = (
+  boardState,
+  clickedSquare,
+  currentTeamsColor
+) => {
+  const clickedRow = boardState[clickedSquare.row];
+  const otherTeamsColor = currentTeamsColor === "black" ? "white" : "black";
+
+  let numberOfPossiblySandwichedSquares = 0;
+  let numberOfSandwichedSquares;
+  let indexOfPossiblySandwichedSquare = clickedSquare.column + 1;
+  while (indexOfPossiblySandwichedSquare < 8) {
+    const colorOfCurrent = clickedRow[indexOfPossiblySandwichedSquare];
+
+    if (
+      colorOfCurrent === otherTeamsColor &&
+      indexOfPossiblySandwichedSquare !== INDEX_OF_FURTHEST_RIGHT_COLUMN
+    ) {
+      numberOfPossiblySandwichedSquares++;
+    } else if (colorOfCurrent === currentTeamsColor) {
+      numberOfSandwichedSquares = numberOfPossiblySandwichedSquares;
+      break;
+    } else if (
+      indexOfPossiblySandwichedSquare === 7 ||
+      colorOfCurrent === null
+    ) {
+      return boardState;
+    }
+    indexOfPossiblySandwichedSquare++;
+  }
+  if (numberOfSandwichedSquares > 0) {
+    boardState[clickedSquare.row][clickedSquare.column] = currentTeamsColor;
+  }
+
+  let indexOfSandwichedSquare = clickedSquare.column + 1;
+  [...Array(numberOfSandwichedSquares)].forEach(() => {
+    boardState[clickedSquare.row][indexOfSandwichedSquare] = currentTeamsColor;
+    indexOfSandwichedSquare++;
+  });
+  return boardState;
+};
