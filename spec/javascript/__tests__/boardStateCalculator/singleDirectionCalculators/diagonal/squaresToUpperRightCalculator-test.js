ef450ee649a1472466175a40910b3a0808940d14
Alex Ispa-Cowan <ispacowanguitar@gmail.com>
2019-03-31 19:01:42 -0700

    more specs for uppder right diagonal calculator

--- spec/javascript/__tests__/boardStateCalculator/singleDirectionCalculators/diagonal/squaresToUpperRightCalculator-test.js
+++ spec/javascript/__tests__/boardStateCalculator/singleDirectionCalculators/diagonal/squaresToUpperRightCalculator-test.js
@@ -34,4 +34,70 @@ describe("flipSandwichedSquaresToUpperRightOfClicked", () => {
       expect(actual).toEqual(expected);
     });
   });
+  describe("with mulitple sandwiched squares", () => {
+    it("updates the board correctly", () => {
+      const initialState = [
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, "bl", null],
+        [null, null, null, null, null, "wh", null, null],
+        [null, null, null, "wh", "wh", null, null, null],
+        [null, null, "wh", "wh", "wh", null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null]
+      ];
+
+      const clickedSquare = { row: 5, column: 2 };
+      const actual = flipSandwichedSquaresToUpperRightOfClicked(
+        initialState,
+        clickedSquare,
+        "bl"
+      );
+
+      const expected = [
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, "bl", null],
+        [null, null, null, null, null, "bl", null, null],
+        [null, null, null, "wh", "bl", null, null, null],
+        [null, null, "wh", "bl", "wh", null, null, null],
+        [null, null, "bl", null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null]
+      ];
+      expect(actual).toEqual(expected);
+    });
+  });
+  describe("with no sandwiched squares", () => {
+    it("updates the board correctly", () => {
+      const initialState = [
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, "wh", "bl", null, null, null],
+        [null, null, "wh", "wh", "wh", null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null]
+      ];
+
+      const clickedSquare = { row: 5, column: 1 };
+      const actual = flipSandwichedSquaresToUpperRightOfClicked(
+        initialState,
+        clickedSquare,
+        "bl"
+      );
+
+      const expected = [
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, "wh", "bl", null, null, null],
+        [null, null, "wh", "wh", "wh", null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null]
+      ];
+      expect(actual).toEqual(expected);
+    });
+  });
 });
